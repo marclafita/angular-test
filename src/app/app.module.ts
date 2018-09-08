3bbcd78e04079bd9045c00ecf422b10f0077a170
Marc Lafita Forcada <marclafitaforcada@Mac-mini-de-Marc.local>
2018-09-09 01:44:49 +0200

    First Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { MenuComponent } from './menu/menu.component';
+import { FilterComponent } from './filter/filter.component';
+import { ColumnComponent } from './column/column.component';
+import { ElementComponent } from './element/element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    MenuComponent,
+    FilterComponent,
+    ColumnComponent,
+    ElementComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['../../../node_modules/materialize-css/dist/css/materialize.css']
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
